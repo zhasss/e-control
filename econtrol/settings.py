a24252098709d3496434ffa7bad4f040045fdd5d
Olzhas Myrzabek <oljasmurzabekm@gmail.com>
2026-01-20 18:44:42 +0500

    vfdv

--- econtrol/settings.py
+++ econtrol/settings.py
@@ -104,14 +104,14 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+
+
+
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = "/data/static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/data/media"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
